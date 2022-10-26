cee7f70b8e5742b9ea447a855dcebc9ab4472850
JimPlauv <jim.plougfelt@gmail.com>
2022-10-26 15:12:35 +0200

    Adding base commands

---
+++ ArchtypeEF.Core/ArchtypeEF.Commands/Commands.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchtypeEF
+{
+    public class Commands : ICommands
+    {
+        public Task Delete<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Get<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task GetAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Inset<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task InsetAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ArchtypeEF.Core/ArchtypeEF.Commands/ICommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchtypeEF
+{
+    public interface ICommands
+    {
+        Task Get<T>();
+        Task Inset<T>();
+        Task Update<T>();
+        Task Delete<T>();
+
+        Task GetAsync<T>();
+        Task InsetAsync<T>();
+        Task UpdateAsync<T>();
+        Task DeleteAsync<T>();
+    }
+}
